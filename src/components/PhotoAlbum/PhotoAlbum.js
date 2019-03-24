2ad71114c27bd68939969362ca8c7a2d602d58c3
Marla Glasser <mbglasser@gmail.com>
2019-03-24 19:34:57 -0400

    Cleanup

--- src/components/PhotoAlbum/PhotoAlbum.js
+++ src/components/PhotoAlbum/PhotoAlbum.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
@@ -6,55 +6,48 @@ import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import Photo from '../Photo';
 import '../../stylesheets/PhotoAlbum.css';
 
-class PhotoAlbum extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { 
-      currentIndex,
-      photos,
-    } = this.props;
-    return (
-      <>
-        <div className="large-photo-row">
-          <div 
-            className={currentIndex > 0 ? 'prev-photo': 'prev-photo disabled'}
-            onClick={this.props.previousPhoto}
-          >
-            <KeyboardArrowLeft />
-          </div>
-          <div>
-            <img 
-              className="big--image" 
-              src={photos[currentIndex].url} 
-              alt={photos[currentIndex].title} 
-            />
-            <div className="large-photo-title">{photos[currentIndex].title}</div>
-          </div>
-          <div 
-            className={currentIndex < photos.length - 1 ? 'next-photo': 'next-photo disabled'}
-            onClick={this.props.nextPhoto}
-          >
-            <KeyboardArrowRight />
-          </div>
-        </div>
-        <div className="thumbs-container">
-          {
-            photos.map((photo, i) =>
-              <Photo 
-                key={photo.id} 
-                photoId={photo.id} 
-                index={i}
-                selected={i === currentIndex} 
-              />
-            )
-          }
-        </div>
-      </>
-    );
-  }
-}
+const PhotoAlbum = ({ 
+  currentIndex, 
+  nextPhoto,
+  photos,
+  previousPhoto,
+}) => (
+  <>
+    <div className="large-photo-row">
+      <div 
+        className={currentIndex > 0 ? 'prev-photo': 'prev-photo disabled'}
+        onClick={previousPhoto}
+      >
+        <KeyboardArrowLeft />
+      </div>
+      <div>
+        <img 
+          className="big--image" 
+          src={photos[currentIndex].url} 
+          alt={photos[currentIndex].title} 
+        />
+        <div className="large-photo-title">{photos[currentIndex].title}</div>
+      </div>
+      <div 
+        className={currentIndex < photos.length - 1 ? 'next-photo': 'next-photo disabled'}
+        onClick={nextPhoto}
+      >
+        <KeyboardArrowRight />
+      </div>
+    </div>
+    <div className="thumbs-container">
+      {
+        photos.map((photo, i) =>
+          <Photo 
+            key={photo.id} 
+            photoId={photo.id} 
+            index={i}
+            selected={i === currentIndex} 
+          />
+        )
+      }
+    </div>
+  </>
+);
 
 export default PhotoAlbum;
